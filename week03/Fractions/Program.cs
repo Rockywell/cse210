cf8ed8bfc8e42b81a883668078bd21752c0fe9b7
Rockywell <P8errockwell@gmail.com>
2025-07-14 16:46:24 -0600

    finished the encapsulation/fractions project

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+
+        Fraction defaultFraction = new Fraction();
+        Fraction wholeFraction = new Fraction(5);
+        Fraction fractionOne = new Fraction(3, 4);
+        Fraction fractionTwo = new Fraction(1, 3);
+
+        List<Fraction> fractionList = [defaultFraction, wholeFraction, fractionOne, fractionTwo];
+
+        foreach (Fraction fraction in fractionList)
+        {
+            Console.WriteLine($"Fraction: {fraction.GetFractionString()}, Decimal: {fraction.GetDecimalValue()}");
+        }
     }
 }
