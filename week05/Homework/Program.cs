c9e29f6227ce42b53736c6690bb415458e50fabf
Rockywell <P8errockwell@gmail.com>
2025-08-01 12:14:41 -0600

    Finished learning activity inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        var assign = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assign.GetSummary());
+
+        var mathAssign = new MathAssignment("Robert Rodriguez", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine($"\n{mathAssign.GetSummary()}\n{mathAssign.GetHomeworkList()}");
+
+        var writingAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine($"\n{writingAssign.GetSummary()}\n{writingAssign.GetWritingInformation()}");
     }
 }
