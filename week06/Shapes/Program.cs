9c57f322fe0ebd58a8b01a08b06aeba6f8d273f5
Rockywell <P8errockwell@gmail.com>
2025-08-11 09:43:32 -0600

    Finished the shapes project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("Hello World! This is the Shapes Project.\n");
+
+        Square square = new Square("Red", 3);
+       
+        square.GetColor();
+        square.GetArea();
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+
+        Circle circle = new Circle("Green", 6);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        shapes.ForEach(shape => {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        });
+
     }
 }
